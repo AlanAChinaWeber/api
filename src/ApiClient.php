0fafbcdce786f6e454ea56bab5eb370f63ccb6ee
sunanzhi <sunanzhi@hotmail.com>
2018-11-07 11:41:38 +0800

    接口入口修改

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -1,15 +1,32 @@
 <?php
 namespace Sunanzhi\Api;
 
-use EasySwoole\Config;
-use GuzzleHttp\Client;
-use function GuzzleHttp\json_decode;
-
 /**
  * 请求api入口
  */
 class ApiClient
 {
+    /**
+     * 命名空间
+     *
+     * @var string
+     */
+    protected static $namespace;
+
+    /**
+     * 模块
+     *
+     * @var string
+     */
+    protected static $module;
+
+    /**
+     * 控制器
+     *
+     * @var string
+     */
+    protected static $moduleClass;
+
     /**
      * 请求接口
      *
@@ -17,27 +34,43 @@ class ApiClient
      * @param string $method 请求接口方法
      * @param mixed ...$args 参数
      * @return mixed
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
-     * @since 2018.10.16
      */
     public static function request(string $url, string $method, ...$args)
     {
-        // 当前命名空间
-        $namespace = (new \ReflectionClass(__CLASS__))->getNamespaceName();
-        // 获取模块
-        $urlExplode = explode('/', $url);
-        $module = $urlExplode[0];
-        $moduleClass = $urlExplode[1];
-
-        // 获取接口参数
-        $apiParams = array_column((new \ReflectionMethod($namespace.'\\'.$module.'\\'.$moduleClass, $method))->getParameters(), 'name');
+        // 获取api参数
+        $apiParams = self::getParams($url, $method);
         // 封装请求参数
         $requestParams = self::packageArgs($apiParams, $args);
         // 获取请求url
-        $apiUrl = Config::getInstance()->getConf('PROVIDERURL.'.$module).'\\'.$module.'\\'.$moduleClass.'\\'.$method;
+        $apiUrl = config('api.PROVIDERURL.' . self::$module) . '/' . self::$module . '/' . self::$moduleClass . '/' . $method;
 
-        return self::guzzleRequest($apiUrl, $requestParams);  
+        return self::guzzleRequest($apiUrl, $requestParams);
+    }
+
+    /**
+     * 获取接口参数
+     *
+     * @param string $url 请求接口路径
+     * @param string $method 请求接口方法
+     * @return array
+     * 
+     * @author sunanzhi <sunanzhi@hotmail.com>
+     */
+    private static function getParams(string $url, string $method): array
+    {
+        // 当前命名空间
+        self::$namespace = (new \ReflectionClass(__CLASS__))->getNamespaceName();
+        // 获取模块
+        $urlExplode = explode('/', $url);
+        self::$module = $urlExplode[0];
+        self::$moduleClass = $urlExplode[1];
+
+        // 获取接口参数
+        $apiParams = array_column((new \ReflectionMethod(self::$namespace . '\\' . self::$module . '\\' . self::$moduleClass, $method))->getParameters(), 'name');
+
+        return $apiParams;
     }
 
     /**
@@ -46,15 +79,14 @@ class ApiClient
      * @param array $apiParams 接口参数
      * @param array $args 请求的参数
      * @return mixed
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
-     * @since 2018.10.16
      */
-    private static function packageArgs(array $apiParams, array $args):array
+    private static function packageArgs(array $apiParams, array $args): array
     {
         $resParams = [];
         $i = 0;
-        foreach($apiParams as $v){
+        foreach ($apiParams as $v) {
             $resParams[$v] = $args[$i];
             $i++;
         }
@@ -68,22 +100,21 @@ class ApiClient
      * @param string $url 请求url
      * @param array $args 请求参数
      * @return mixed
-     * 
+     *
      * @author sunanzhi <sunanzhi@hotmail.com>
-     * @since 2018.10.16
      */
     private static function guzzleRequest(string $url, array $args)
     {
-        $guzzleClient = new Client();
-        $response = $guzzleClient->request('GET', $url, $args);
-        $content = (string)$response->getBody();
+        $guzzleClient = new \GuzzleHttp\Client();
+        $response = $guzzleClient->request('POST', $url, $args);
+        $content = (string) $response->getBody();
 
         $object = '';
 
-        if('' != $content){
+        if ('' != $content) {
             $object = json_decode($content, true);
         }
-        
+
         return $object;
     }
 }
